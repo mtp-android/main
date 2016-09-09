db7f152daa7f6b4541b242e21abf00c03dda6e0d
lmedrzycki <medrzycki.lukasz@gmail.com>
2016-09-09 11:01:57 +0200

    unicloud access

--- app/src/main/java/mgr/mtp/Utils/Constants.java
+++ app/src/main/java/mgr/mtp/Utils/Constants.java
@@ -8,13 +8,13 @@ import java.text.SimpleDateFormat;
 public class Constants {
 
     // on emulator
-    public static String host = "http://10.0.2.2:8081";
+    //public static String host = "http://10.0.2.2:8081";
 
     // on device - router
     //public static String host = "http:/169.168.7.113:8081";
 
     // on device
-    //public static String host = "http://env-9479195.unicloud.pl/mtp";
+    public static String host = "http://env-6137508.unicloud.pl/mtp";
 
     public static SimpleDateFormat queryDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     public static SimpleDateFormat displayDateFormat = new SimpleDateFormat("dd-MM-yyyy");
