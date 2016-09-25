fe3d3f0e35b39a5005719c79a57403a0ebbff75c
lmedrzycki <lmedrzycki.jamiru.pl>
2016-09-25 21:30:53 +0200

    Zapotrzebowanie kaloryczne fix

--- app/src/main/java/mgr/mtp/Diet/DietSettings.java
+++ app/src/main/java/mgr/mtp/Diet/DietSettings.java
@@ -62,46 +62,79 @@ public class DietSettings extends PreferenceActivity {
         int target = Integer.parseInt(prefs.getString("user_target", "1"));
         int height = Integer.parseInt(prefs.getString("user_height", "170"));
 
-        double calories = 0, proteins, carbs, fat;
 
-        switch (gender) {
+        // Na podstawie Anita Bean [2008]
+        // Algorytm obliczania zapotrzebowania kalorycznego
+
+        double calories, proteins, carbs, fat, rmr, factorA = 0, factorB = 0;
+        boolean isMan;
+
+        // Obliczanie tempa metabolizmu spoczynkowego (RMR)
+        isMan = gender == 1;
+
+        if(age <= 18){
+            factorA = isMan ? 17.5 : 12.2;
+            factorB = isMan ? 651 : 746;
+
+        } else if(age > 18 && age <= 30){
+            factorA = isMan ? 15.3 : 14.7;
+            factorB = isMan ? 679 : 496;
+
+        } else if(age > 30 && age <= 60){
+            factorA = isMan ? 11.6 : 8.7;
+            factorB = isMan ? 879 : 829;
+        }
+
+        rmr = (weight * factorA) + factorB;
+
+        // Obliczanie dziennego wydatku kalorycznego
+        // 1 - brak aktywnosci
+        // 2 - umiarkowana aktywnosc
+        // 3 - bardzo aktywny
+
+        switch (activityLevel) {
             case 1:
-                calories = 66.5 + (13.7 * weight) + (5 * height) - (6.8 * age);
+                rmr = rmr * 1.4;
                 break;
             case 2:
-                calories = 655 + (9.6 * weight) + (1.85 * height) - (4.7 * age);
+                rmr = rmr * 1.7;
+                break;
+            case 3:
+                rmr = rmr * 2.0;
                 break;
         }
 
-        switch (activityLevel) {
+        // Wydatek energetyczny podczas ćwiczeń
+        // przy zalozeniu 1h treningu x 3 dni w tygodniu
+        // 1h treningu = srednio 360 kcal
+        calories = rmr + (3 * 360);
+
+        // Uwzglednienie celu dietetycznego
+        // 1 - redukcja
+        // 2 - wzrost masy miesniowej
+        // 3 - utrzymanie wagi
+
+        switch (target) {
             case 1:
                 calories = calories * 1.2;
                 break;
             case 2:
-                calories = calories * 1.6;
-                break;
-            case 3:
-                calories = calories * 1.8;
-                break;
-        }
-
-
-        switch (target) {
-            case 1:
-                calories = calories - 300;
-                break;
-            case 2:
-                calories = calories + 300;
+                calories = calories * 0.85;
                 break;
             case 3:
                 break;
         }
 
-        proteins = calories * 0.20;
-        fat = calories * 0.25;
-        carbs = (calories - (proteins + fat));
+        // Udzial poszczegolnych makroskladnikow
+        // Bialko 1.6 g na kazdy kilogram masy ciala
+        // Tluszcze 25% zapotrzebowania kalorycznego
+        // Weglowodany - pozostala ilosc
 
-        proteins = proteins / 4;
+        proteins = weight * 1.6;
+        fat = (calories * 0.25);
+        carbs = (calories - ((proteins * 4) + fat));
+
+        // Przeliczenie z kcal na g
         fat = fat / 9;
         carbs = carbs / 4;
 
